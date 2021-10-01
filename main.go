b393aa83ef52c55c2995afd8a607ca64449f66fb
M. Andrew Eick <maeick@missionfocus.com>
2021-10-01 17:39:49 -0400

    catchup

--- main.go
+++ main.go
@@ -1,6 +1,128 @@
 // main.go
 package main
 
+import (
+	"log"
+	"os"
+	"time"
+
+	f "github.com/fauna/faunadb-go/v4/faunadb"
+)
+
+var (
+	secret   = os.Getenv("FAUNADB_SECRET")
+	endpoint = f.Endpoint("https://db.us.fauna.com")
+
+	client = f.NewFaunaClient(secret, endpoint)
+
+	dbName = "emms"
+)
+
+/*
+ * Check for the existencse of the database.  If it exists, return true,
+ * otherwise create it.
+ */
+func createDatabase() {
+	res, err := client.Query(
+		f.If(
+			f.Exists(f.Database(dbName)),
+			true,
+			f.CreateDatabase((f.Obj{"name": dbName}))))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if res != f.BooleanV(true) {
+		log.Printf("Created Database: %s\n", dbName)
+	} else {
+		log.Printf("Database: %s, Already Exists\n", dbName)
+	}
+}
+
+func getDbClient() (dbClient *f.FaunaClient) {
+	var res f.Value
+	var err error
+	var dbSecret string
+
+	res, err = client.Query(
+		f.CreateKey(f.Obj{
+			"database": f.Database(dbName),
+			"role":     "server",
+		}))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = res.At(f.ObjKey("secret")).Get(&dbSecret)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Printf("Database: %s, specific key: %s\n", dbName, dbSecret)
+
+	dbClient = client.NewSessionClient(dbSecret)
+
+	return
+}
+
+func createCollection(collectionName string, dbClient *f.FaunaClient) {
+
+	res, err := dbClient.Query(
+		f.If(
+			f.Exists(f.Collection(collectionName)),
+			true,
+			f.CreateCollection(f.Obj{"name": collectionName})))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if res != f.BooleanV(true) {
+		log.Printf("Created Collection: %s\n", collectionName)
+	} else {
+		log.Printf("Collection: %s, Already Exists\n", collectionName)
+	}
+}
+
 func main() {
-	println("Ba dum, tss!")
+
+	createDatabase()
+	dbClient := getDbClient()
+	createCollection("photographs", dbClient)
+
+	pmd := PhotoMetaData{
+		Name:                "test.jpg",
+		ParsedName:          "testParsedName",
+		Artist:              "testArtist",
+		CaptureTime:         time.Now(),
+		CaptureYear:         "2020",
+		CaptureYearMonth:    "2020-01",
+		CaptureYearMonthDay: "2020-01-01",
+		Description:         "testDescription",
+		Caption:             "testCaption",
+		ID:                  1,
+		Height:              100,
+		Width:               100,
+	}
+
+	_, err := dbClient.Query(
+		f.Create(
+			f.Collection("photographs"),
+			f.Obj{"data": pmd},
+		),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// res, err := client.Query(f.Get(f.Ref(f.Collection("products"), "202")))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// log.Println(res)
 }
---
+++ pmd.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	_ "image/jpeg"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/tiff"
+)
+
+type Labels struct {
+	Name       string
+	Confidence float64
+}
+
+type PhotoMetaData struct {
+	Name                string
+	ParsedName          string
+	Artist              string
+	CaptureTime         time.Time
+	CaptureYear         string
+	CaptureYearMonth    string
+	CaptureYearMonthDay string
+	Description         string
+	Caption             string
+	ID                  uint64
+	Height              int
+	Width               int
+	// Classification      struct {
+	// 	Labels []Labels
+	// }
+}
+
+func getCleanExifValue(md *tiff.Tag) string {
+	if md == nil {
+		return ""
+	}
+	s := fmt.Sprintf("%v", md)
+
+	if len(s) > 0 && s[0] == '"' {
+		s = s[1:]
+	}
+	if len(s) > 0 && s[len(s)-1] == '"' {
+		s = s[:len(s)-1]
+	}
+	return s
+}
+
+func populatePMD(filepath string) *PhotoMetaData {
+	fileBytes, err := os.Open(filepath)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	defer fileBytes.Close()
+
+	x, err := exif.Decode(fileBytes)
+
+	if err != nil {
+		fmt.Print("should not get an error")
+	}
+
+	var pmd *PhotoMetaData = new(PhotoMetaData)
+
+	exifValueArtist, err := x.Get(exif.Artist)
+
+	if err != nil {
+		fmt.Print("error decoding the Artist")
+	}
+
+	pmd.Artist = getCleanExifValue(exifValueArtist)
+
+	pmd.CaptureTime, err = x.DateTime()
+
+	if err != nil {
+		fmt.Print("error decodeing the time")
+	}
+
+	// `Format` and `Parse` use example-based layouts. Usually
+	// you'll use a constant from `time` for these layouts, but
+	// you can also supply custom layouts. Layouts must use the
+	// reference time `Mon Jan 2 15:04:05 MST 2006` to show the
+	// pattern with which to format/parse a given time/string.
+	// The example time must be exactly as shown: the year 2006,
+	// 15 for the hour, Monday for the day of the week, etc.
+	pmd.CaptureYear = pmd.CaptureTime.Format("2006")
+	pmd.CaptureYearMonth = pmd.CaptureTime.Format("2006-01")
+	pmd.CaptureYearMonthDay = pmd.CaptureTime.Format("2006-01-02")
+
+	exifValueDescription, err := x.Get(exif.ImageDescription)
+
+	if err != nil {
+		fmt.Print("error decoding the description")
+	}
+
+	pmd.Description = getCleanExifValue(exifValueDescription)
+
+	exifVaultWidth, _ := x.Get(exif.ImageWidth)
+	pmd.Width, _ = strconv.Atoi(getCleanExifValue(exifVaultWidth))
+
+	exifVaultLength, _ := x.Get(exif.ImageLength) // Image height called
+	pmd.Height, _ = strconv.Atoi(getCleanExifValue(exifVaultLength))
+
+	return pmd
+}
